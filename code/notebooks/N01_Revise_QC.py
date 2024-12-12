14e3080d67022864a3e1ab8bbc3e59bf008028b1
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2024-12-12 11:24:49 -0500

    new function to read outputs of afni gen_ss_review_table added

--- code/notebooks/N01_Revise_QC.py
+++ code/notebooks/N01_Revise_QC.py
@@ -17,48 +17,56 @@
 #
 # This notebook will use the outcome of AFNI QC reports to decide which scans we keep and which scans we will not include in sucessive analyses.
 #
+# Pre-processing of this data is done via the following scripts:
+#
+# * ```S00a_Freesurfer.CreateSwarm.sh```: runs freesurfer recon-all in all subjects
+# * ```S00b_Freesurfer2AFNI.CreateSwarm.sh```: transform freesurfer outcomes into AFNI/SUMA files
+# * ```S01_Prroc_Anat.CreateSwarm.sh```: computes spatial transformations needed to brain each subject anatomical into MNI space
+# * ```S02_Afni_Preproc_ses?.CreateSwarm.sh```: fully pre-processes the data using afni_proc.py and tedana
+#
+# Once all data has been pre-processed, this script will make use of AFNI QC tools to select scans that passed a series of criteria to be included in the remainder of the analyses
+
 # The following ```get_ss_review_table``` command will generate two tables:
 #
 # * ```review_all_scans.txt```: contains summary QC values for all scans that completed afni_proc.py
-# * ```review_keepers.txt```: contains information only for scans that pass out QC criteria.
+# * ```review_keepers.txt```: contains information only for scans that pass our QC criteria.
 #
 # Our QC criteria is:
 #
-# 1. Only include scans from the GE scanner, becuase these seem to have slice timing information properly encoded (as opposed to the Siemens ones that do not): ```-report_outliers 'censor fraction' GE 0.1```
+# 1. Only include scans from the GE scanner, becuase these seem to have slice timing information properly encoded (as opposed to the Siemens ones that do not): ```-report_outliers 'slice timing pattern' EQ simult```
 # 2. Remove scans for which the EPI to Anat failed. Upon visual inspection we deem anat/EPI Dice <=.88 to reflect errors: ```-report_outliers 'anat/EPI mask Dice coef' LT 0.88```
 # 3. Remove any scan that required an L/R Flip (None did, but nevertheless we leave the condition here): ```-report_outliers 'flip guess' EQ FLIP```
 # 4. Remove any scan with dimensions different from those of the first scan (a few scans had voxel size slightly bigger (.001mm)). Most likely not an issue, but just to be on the safe side: ```-report_outliers 'orig voxel resolution' VARY```
-# 5. Remove any scan with a fraction of censored TRs greater than 0.1 mm: ```-report_outliers 'censor fraction' GT 0.1```
-#
+# 5. For now, we do not remove data based on motion. The idea being, that we want to have both good and bad data to look at.
+#    
 # ```bash
 # ml afni
 # # cd /data/SFIMJGC_HCP7T/BCBL2024/prcs_data
 # gen_ss_review_table.py -overwrite \
-#                        -report_outliers 'anat/EPI mask Dice coef' LT 0.88 \
-#                        -report_outliers 'slice timing pattern' EQ simult \
-#                        -report_outliers 'flip guess' EQ FLIP \
-#                        -report_outliers 'orig voxel counts' VARY \
-#                        -report_outliers 'orig voxel resolution' VARY \
-#                        -report_outliers 'TR' VARY \
-#                        -report_outliers 'num TRs per run' VARY \
-#                        -report_outliers 'average motion (per TR)' SHOW \
-#                        -report_outliers 'max motion displacement' SHOW \
-#                        -report_outliers 'max censored displacement' SHOW \
-#                        -report_outliers 'max motion displacement'   SHOW \
-#                        -report_outliers 'num TRs per run (applied)' SHOW \
-#                        -report_outliers 'TSNR average' SHOW \
-#                        -report_outliers 'global correlation (GCOR)' SHOW \
-#                        -report_outliers 'anat/templ mask Dice coef' SHOW \
-#                        -report_outliers 'censor fraction' SHOW \
-#                        -write_table review_all_scans.txt \
-#                        -write_outliers review_keepers.txt -show_keepers \
-#                        -infiles ./sub-*/D02_Preproc_fMRI_ses-?/out.ss_review.*
+#                         -report_outliers 'anat/EPI mask Dice coef' LT 0.88 \
+#                         -report_outliers 'slice timing pattern' EQ simult \
+#                         -report_outliers 'flip guess' EQ FLIP \
+#                         -report_outliers 'orig voxel counts' VARY \
+#                         -report_outliers 'orig voxel resolution' VARY \
+#                         -report_outliers 'TR' VARY \
+#                         -report_outliers 'num TRs per run' VARY \
+#                         -report_outliers 'echo times' VARY \
+#                         -report_outliers 'average motion (per TR)' SHOW \
+#                         -report_outliers 'max censored displacement' SHOW \
+#                         -report_outliers 'max motion displacement'   SHOW \
+#                         -report_outliers 'num TRs per run (applied)' SHOW \
+#                         -report_outliers 'TSNR average' SHOW \
+#                         -report_outliers 'global correlation (GCOR)' SHOW \
+#                         -report_outliers 'anat/templ mask Dice coef' SHOW \
+#                         -report_outliers 'censor fraction' SHOW \
+#                         -write_table review_all_scans.txt \
+#                         -write_outliers review_keepers.txt -show_keepers \
+#                         -infiles ./sub-*/D02_Preproc_fMRI_ses-?/out.ss_review.*
 # ```
 #
 # Not using this for now, as we want to have all possible motion here
 #
 #                        -report_outliers 'censor fraction' GT 0.1 \
-#
 
 # Data from this dataset was acquired in two different scanners:
 #
@@ -68,77 +76,53 @@
 import pandas as pd
 import numpy as np
 import hvplot.pandas
+import os.path as osp
+from utils.basics import PRJ_DIR
 
-report_summary_path  = '/data/SFIMJGC_HCP7T/BCBL2024/prcs_data/review_all_scans.txt'
-report_keepers_path  = '/data/SFIMJGC_HCP7T/BCBL2024/prcs_data/review_keepers.txt'
+# allows visualisation in notebook
+from bokeh.io import output_notebook
+from bokeh.resources import INLINE
+output_notebook(INLINE)
+from utils.basics import read_gen_ss_review_table
+
+# + active=""
+# import sys
+# sys.path.append('/usr/local/apps/afni/current-py3/linux_rocky_8/')
+# from afnipy.lib_gssrt import 
+# -
+
+report_summary_path  = osp.join(PRJ_DIR,'prcs_data','review_all_scans.txt')
+report_keepers_path  = osp.join(PRJ_DIR,'prcs_data','review_keepers.txt')
 
 # # 1. Load the Full Report
 #
 # Sometimes we will want to compare what we keep to the full sample. It is for these purposes that we load the full report
 
-# +
-report_summary_df = pd.read_csv(report_summary_path, sep='\t')
-report_summary_df = report_summary_df.infer_objects().drop(0)
-# Correct Columns (e.g., removed UnNamed)
-initial_col_name = list(report_summary_df.columns)
-new_col_name     = []
-for col in initial_col_name:
-    if 'Unnamed' in col:
-        new_col_name.append(new_col_name[-1])
-    else:
-        new_col_name.append(col)
-report_summary_df.columns = new_col_name
-# Assign Correct Type
-report_summary_df['num TRs above mot limit']   = report_summary_df['num TRs above mot limit'].astype(int)
-report_summary_df['TSNR average']              = report_summary_df['TSNR average'].astype(float)
-report_summary_df['average motion (per TR)']   = report_summary_df['average motion (per TR)'].astype(float)
-report_summary_df['max motion displacement']   = report_summary_df['max motion displacement'].astype(float)
-report_summary_df['max censored displacement'] = report_summary_df['max censored displacement'].astype(float)
-report_summary_df['anat/EPI mask Dice coef']   = report_summary_df['anat/EPI mask Dice coef'].astype(float)
-report_summary_df['anat/templ mask Dice coef'] = report_summary_df['anat/templ mask Dice coef'].astype(float)
-report_summary_df['global correlation (GCOR)'] = report_summary_df['global correlation (GCOR)'].astype(float)
-
-print(report_summary_df.shape)
-report_summary_df.head(5)
-# -
-
-print(list(report_summary_df.columns.unique()))
+report_summary_df = read_gen_ss_review_table(report_summary_path)
 
 # ***
 #
 # # 2. Load the list of scans that passed QC criteria
 
-# +
-report_keepers_df = pd.read_csv(report_keepers_path, sep='\t')
-outlier_criteria  = report_keepers_df.iloc[0]
-report_keepers_df = report_keepers_df.drop(0)
-report_keepers_df['average motion (per TR)']   = report_keepers_df['average motion (per TR)'].astype(float)
-report_keepers_df['max motion displacement']   = report_keepers_df['max motion displacement'].astype(float)
-report_keepers_df['max censored displacement'] = report_keepers_df['max censored displacement'].astype(float)
-report_keepers_df['TSNR average']              = report_keepers_df['TSNR average'].astype(float)
-report_keepers_df['TSNR average']              = report_keepers_df['TSNR average'].astype(float)
-report_keepers_df['anat/EPI mask Dice coef']   = report_keepers_df['anat/EPI mask Dice coef'].astype(float)
-report_keepers_df['anat/templ mask Dice coef'] = report_keepers_df['anat/templ mask Dice coef'].astype(float)
-report_keepers_df['global correlation (GCOR)'] = report_keepers_df['global correlation (GCOR)'].astype(float)
-
-print(report_keepers_df.shape)
+report_keepers_df = read_gen_ss_review_table(report_keepers_path)
 report_keepers_df.head(5)
-# -
 
 good_scan_ids = list(report_keepers_df['infile'])
 print("++ Number of selected scans: %d scans" % len(good_scan_ids))
 
 # # 3. Check motion
 
-report_keepers_df.hvplot.hist('average motion (per TR)', bins=np.linspace(0,0.4,50), label='keepers', normed=True, alpha=.5, title='Average Motion (per TR)') * \
-report_summary_df['average motion (per TR)'].hvplot.hist('average motion (per TR)', bins=np.linspace(0,0.4,50), label='all', alpha=.5, normed=True)
+a = report_keepers_df.hvplot.kde('average motion (per TR)',label='Passed QC', alpha=.5, title='Average Motion (per TR)') * report_summary_df.hvplot.kde('average motion (per TR)', label='All scans', alpha=.5)
+b = report_keepers_df.hvplot.hist('average motion (per TR)', bins=np.linspace(0,0.4,50), label='Passed QC', normed=True, alpha=.5, title='Average Motion (per TR)') * \
+report_summary_df['average motion (per TR)'].hvplot.hist('average motion (per TR)', bins=np.linspace(0,0.4,50), label='All scans', alpha=.5, normed=True)
+a+b
 
-report_keepers_df.hvplot.hist('max motion displacement', bins=np.linspace(0,6,50), label='keepers', normed=True, alpha=.5, title='Maximum Motion Displacement') * \
+a = report_keepers_df.hvplot.kde('max motion displacement',label='Passed QC', alpha=.5, title='Maximum Motion Displacement') * report_summary_df.hvplot.kde('max motion displacement', label='All scans', alpha=.5)
+b = report_keepers_df.hvplot.hist('max motion displacement', bins=np.linspace(0,6,50), label='keepers', normed=True, alpha=.5, title='Maximum Motion Displacement') * \
 report_summary_df['max motion displacement'].hvplot.hist('max motion displacement', bins=np.linspace(0,6,50), label='all', alpha=.5, normed=True)
+a+b
 
 # Most likely we will use 'max censored displacement' as the estimate of motion when doing the Powers et al. business. As according to AFNI Discourse (https://discuss.afni.nimh.nih.gov/t/max-motion-displacement-question/2754/3)... "The censored displacement just considers time points that were not removed, those that are still in the time series after censoring. It is more useful, since those are the time points that will be considered in the regression."
-
-report_keepers_df.hvplot.scatter(x='max motion displacement', y='max censored displacement', hover_cols=['infile'])
 
 # # 4. Check TSNR
 
@@ -155,6 +139,8 @@ report_keepers_df.hvplot.hist('anat/templ mask Dice coef', title='Quality of Tra
 
 report_keepers_df.hvplot.hist('global correlation (GCOR)', title='global correlation (GCOR)')
 
+report_keepers_df[['e01','e02','e03']].hvplot.box()
+
 # # 7. Write final list of scans to disk
 
 sbj_idx = [i.split('/')[1] for i in report_keepers_df['infile'].values]
@@ -163,6 +149,8 @@ report_keepers_df.index = pd.MultiIndex.from_arrays([sbj_idx,ses_idx],names=['Su
 
 report_keepers_df.to_csv('../../../resources/good_scans.txt')
 
-gen_ss_review_scripts.py -show_uvar_dict
+report_keepers_df.set_index('subject ID').loc['sub-237']
+
+report_keepers_df.loc['sub-237',:]
 
 
--- code/notebooks/utils/basics.py
+++ code/notebooks/utils/basics.py
@@ -1,4 +1,6 @@
 import os.path as osp
+import pandas as pd
+import numpy as np
 
 PRJ_DIR       = '/data/SFIMJGC_HCP7T/BCBL2024/'
 ATLAS_NAME    = 'Schaefer2018_400Parcels_17Networks'
@@ -7,4 +9,63 @@ PRCS_DATA_DIR = osp.join(PRJ_DIR,'prcs_data')
 ATLASES_DIR   = osp.join(PRJ_DIR,'atlases')
 CODE_DIR      = osp.join(PRJ_DIR,'me_staticfc','code')
 
+# Echo Time information for the Spreng Dataset
+TES_MSEC_PER_SCANNER = {'1':{'e01':13.7,'e02':30,'e03':47},
+                        '2':{'e01':14,'e02':29.96,'e03':45.92}}
+
+# We are only working with data from Site 1
+TES_MSEC = TES_MSEC_PER_SCANNER['1']
+
 Power264_cmap = {''}
+
+def read_gen_ss_review_table(file_path):
+    """
+    Reads the output of AFNI command gen_ss_review_table and
+    organizes the contents into a pandas dataframe with meaningful
+    column names
+    
+    Input
+    -----
+    file_path: path to input file
+    
+    Returns
+    -------
+    df: dataframe with the information in the file
+    """
+    if not osp.exists(file_path):
+        print('++ERROR [read_gen_ss_review_table]: input file does not exists')
+        return None
+    df = pd.read_csv(file_path, sep='\t', header=None)
+    original_columns  = [(a,b) for a,b in df.loc[[0,1]].values.T]
+    df.drop([0,1], inplace=True)
+    new_columns = []
+    for (a,b) in original_columns:
+        if a=='infile':
+            new_columns = new_columns + ['infile']
+            continue
+        if (a=='echo times'):
+            new_columns = new_columns + ['e01','e02','e03']
+            continue
+        if (a is np.nan):
+            continue
+        if (a=='orig voxel counts'):
+            new_columns = new_columns + ['Nx','Ny','Nz']
+            continue
+        if (a=='orig voxel resolution'):
+            new_columns = new_columns + ['orig Dx','orig Dy','orig Dz']
+            continue
+        if (a=='final voxel resolution'):
+            new_columns = new_columns + ['final Dx','final Dy','final Dz']
+            continue
+        if (a=='orig volume center'):
+            new_columns = new_columns + ['orig volume center x','orig volume center y','orig volume center z']
+            continue
+        new_columns = new_columns + [a]
+    df.columns = new_columns
+    for c in df.columns:
+        try:
+            df[c] = df[c].astype(float)
+        except:
+            df[c] = df[c]
+    print("++ INFO [read_gen_ss_review_table]: Number of scans = %d | Number of metrics per scan = %d" % (df.shape))
+    return df

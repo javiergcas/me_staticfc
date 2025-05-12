6ba7b72029501c475695c32d2f4423c6e4b79628
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2025-05-12 14:12:19 -0400

    New jupytext_version line

--- code/notebooks/N03a_Basic_and_GSR_pipeline_and_ROIextract.py
+++ code/notebooks/N03a_Basic_and_GSR_pipeline_and_ROIextract.py
@@ -6,7 +6,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.15.2
+#       jupytext_version: 1.16.1
 #   kernelspec:
 #     display_name: BOLD WAVES 2024a
 #     language: python

c05aef7626dfab0d61f0ffa2a83368ceb320c18c
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2025-08-04 09:53:26 -0400

    N01b now creates a secondary swarm file for re-running jobs that failed

--- code/notebooks/N01b_tedana_robustICA_pipeline.evaluation.py
+++ code/notebooks/N01b_tedana_robustICA_pipeline.evaluation.py
@@ -48,7 +48,7 @@ print(log_path)
 
 with open(script_path, 'w') as the_file:
     the_file.write('# Script Creation Date: %s\n' % str(datetime.date.today()))
-    the_file.write(f'# swarm -f {script_path} -g 64 -t 8 -b 4 --time 00:50:00 --logdir {log_path} --partition quick,norm --module afni\n')
+    the_file.write(f'# swarm -f {script_path} -g 64 -t 8 -b 2 --time 01:50:00 --logdir {log_path} --partition quick,norm --module afni\n')
     the_file.write('\n')
     for sbj,ses in list(dataset_info_df.index):
         the_file.write(f'export SBJ={sbj} SES={ses} DATASET=evaluation NORDIC=on; sh  {CODE_DIR}/bash/S08_tedana-robustICA.sh \n')
@@ -59,7 +59,7 @@ script_path
 
 # ```bash
 # # cd /data/SFIMJGC_HCP7T/BCBL2024/swarm.javiergc
-# swarm -f /data/SFIMJGC_HCP7T/BCBL2024/swarm.javiergc/S08_tedana-robustICA_evaluation.SWARM.sh -g 64 -t 8 -b 10 --time 00:20:00 --logdir /data/SFIMJGC_HCP7T/BCBL2024/logs.javiergc/S08_tedana-robustICA_evaluation.log --partition quick,norm --module afni
+# swarm -f /data/SFIMJGC_HCP7T/BCBL2024/swarm.javiergc/S08_tedana-robustICA_evaluation.SWARM.sh -g 64 -t 8 -b 2 --time 01:50:00 --logdir /data/SFIMJGC_HCP7T/BCBL2024/logs.javiergc/S08_tedana-robustICA_evaluation.log --partition quick,norm --module afni
 # ```
 #
 # ***
@@ -68,13 +68,20 @@ script_path
 
 # %%time
 missing_cases = []
-for sbj,ses in list(dataset_info_df.index):
-    for NORDIC in ['on','off']:
-        for e in ['e01','e02','e03']:
-            path = osp.join(PRCS_DATA_DIR,sbj,f'D03_Preproc_{ses}_NORDIC-{NORDIC}','tedana_fastica','tedana_report.html')
-            if not osp.exists(path):
-                print('++ WARNING: %s is missing'% path)
-                missing_cases.append((sbj,ses))
+script_path = osp.join(PRJ_DIR,f'swarm.{username}',f'S08_tedana-robustICA_evaluation.SWARM.failed.sh')
+print(script_path)
+with open(script_path, 'w') as the_file:
+    the_file.write('# Script Creation Date: %s\n' % str(datetime.date.today()))
+    the_file.write(f'# swarm -f {script_path} -g 64 -t 4 --time 08:00:00 --logdir {log_path} --module afni\n')
+    
+    for sbj,ses in list(dataset_info_df.index):
+        for NORDIC in ['on','off']:
+                path = osp.join(PRCS_DATA_DIR,sbj,f'D03_Preproc_{ses}_NORDIC-{NORDIC}','tedana_robustica','tedana_report.html')
+                if not osp.exists(path):
+                    print('++ WARNING: %s is missing'% path)
+                    missing_cases.append((sbj,ses))
+                    the_file.write(f'export SBJ={sbj} SES={ses} DATASET=evaluation NORDIC={NORDIC}; sh  {CODE_DIR}/bash/S08_tedana-robustICA.sh \n')
+print(len(missing_cases))
 
 ' '.join([f'-e "export SBJ={sbj} SES={ses}"' for sbj, ses in list(set(missing_cases))])
 

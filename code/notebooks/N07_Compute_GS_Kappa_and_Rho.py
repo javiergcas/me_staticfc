65cd5ea185bf9b4a4490312b7e17b38718d022be
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2025-08-22 09:08:54 -0400

    Script updated to work on new folder structure

--- code/notebooks/N07_Compute_GS_Kappa_and_Rho.py
+++ code/notebooks/N07_Compute_GS_Kappa_and_Rho.py
@@ -24,14 +24,17 @@ import os
 from tqdm import tqdm
 import datetime
 from utils.basics import PRJ_DIR, PRCS_DATA_DIR, SPRENG_DOWNLOAD_DIR, CODE_DIR
+from utils.basics import get_dataset_index
 
 import getpass
 username = getpass.getuser()
 print(username)
 
-dataset_info_df = pd.read_csv(osp.join(PRJ_DIR,'resources','good_scans.txt'))
-dataset_info_df = dataset_info_df.set_index(['Subject','Session'])
-print('++ Number of scans: %s scans' % dataset_info_df.shape[0])
+DATASET = 'evaluation'
+
+ds_index = get_dataset_index(DATASET)
+ses_list = list(ds_index.get_level_values('Session').unique())
+sbj_list = list(ds_index.get_level_values('Subject').unique())
 
 # # 1. Create batch jobs for computing kappa and rho for all scans
 #
@@ -39,12 +42,12 @@ print('++ Number of scans: %s scans' % dataset_info_df.shape[0])
 #
 # ## 1.1. Create the path to the swarm file
 
-script_path = osp.join(PRJ_DIR,f'swarm.{username}',f'N10_Compute_GS_kappa_and_rho.SWARM.sh')
+script_path = osp.join(PRJ_DIR,f'swarm.{username}',f'N07_Compute_GS_kappa_and_rho.SWARM.sh')
 print(script_path)
 
 # ## 1.2. Create a folder for log files
 
-log_path = osp.join(PRJ_DIR,f'logs.{username}',f'N10_Compute_GS_kappa_and_rho.log')
+log_path = osp.join(PRJ_DIR,f'logs.{username}',f'N07_Compute_GS_kappa_and_rho.log')
 if not osp.exists(log_path):
     os.makedirs(log_path)
 print(log_path)
@@ -57,7 +60,7 @@ with open(script_path, 'w') as the_file:
     the_file.write('# Script Creation Date: %s\n' % str(datetime.date.today()))
     the_file.write(f'# swarm -f {script_path} -g 8 -t 8 -b 20 --time 00:10:00 --logdir {log_path} --partition quick,norm --module afni \n')
     the_file.write('\n')
-    for sbj,ses in tqdm(dataset_info_df.index):
+    for sbj,ses in tqdm(ds_index):
         the_file.write(f'export SBJ={sbj} RUN={ses}; cd {CODE_DIR}/python/; sh {CODE_DIR}/python/GS_kappa_and_rho.sh \n')
 the_file.close()     
 
@@ -70,9 +73,9 @@ the_file.close()
 #
 # We will be able to load that in other notebooks.
 
-kappa_rho_df = pd.DataFrame(index=dataset_info_df.index,columns=['kappa (GS)','rho (GS)','kappa_rho_color'])
-for sbj,ses in tqdm(dataset_info_df.index):
-    path = osp.join(PRCS_DATA_DIR,sbj,f'D02_Preproc_fMRI_{ses}',f'{sbj}_{ses}_GS_kappa_and_rho.txt')
+kappa_rho_df = pd.DataFrame(index=ds_index,columns=['kappa (GS)','rho (GS)','kappa_rho_color'])
+for sbj,ses in tqdm(ds_index):
+    path = osp.join(PRCS_DATA_DIR,sbj,f'D03_Preproc_{ses}_NORDIC-off',f'{sbj}_{ses}_GS_kappa_and_rho.txt')
     if not osp.exists(path):
         print("++ WARNING: GS Kappa/Rho file missing: %s" % path)
         continue
@@ -101,12 +104,12 @@ import hvplot.pandas
 from scipy.stats import zscore
 from tqdm import tqdm
 
-tes = list(TES_MSEC['Spreng_Scanner1'].values())
+tes = list(TES_MSEC['evaluation'].values())
 ne  = len(tes)
 sbj,ses='sub-01','ses-1'
 
 # Load the adaptive mask
-mask_path = osp.join(PRCS_DATA_DIR,sbj,f'D02_Preproc_fMRI_{ses}','tedana_r01','adaptive_mask.nii.gz')
+mask_path = osp.join(PRCS_DATA_DIR,sbj,f'D03_Preproc_{ses}_NORDIC-off','tedana_fastica','adaptive_mask.nii.gz')
 mask_img  = nib.load(mask_path)
 mask_data = mask_img.get_fdata()
 nx,ny,nz  = mask_data.shape

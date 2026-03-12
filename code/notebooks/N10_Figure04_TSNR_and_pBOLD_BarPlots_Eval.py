b041cc949badfb16c22411a236c543ed207da32c
Javier Gonzalez-Castillo <javiergcas@gmail.com>
2026-03-12 15:39:18 -0400

    N10 now also generates two panels of Supp. Fig. 3

--- code/notebooks/N10_Figure04_TSNR_and_pBOLD_BarPlots_Eval.py
+++ code/notebooks/N10_Figure04_TSNR_and_pBOLD_BarPlots_Eval.py
@@ -5,7 +5,7 @@
 # 
 # This notebook will load all TSNR and pBOLD estimates for the evaluation dataset and create summary bar plots with statistics
 
-# In[18]:
+# In[1]:
 
 
 import os.path as osp
@@ -39,7 +39,7 @@ print('++ Reading pBOLD from %s' % pBOLD_path)
 
 # Load the xr DataArray with the pBOLD estimates
 
-# In[19]:
+# In[4]:
 
 
 pBOLD = xr.open_dataarray(pBOLD_path)
@@ -47,7 +47,7 @@ pBOLD = xr.open_dataarray(pBOLD_path)
 
 # Extract only the scan-level pBOLD values and place them into a dataframe structured ready for plotting purposes
 
-# In[20]:
+# In[5]:
 
 
 pBOLD = pBOLD.sel(fc_metric=FC_METRIC,ee_vs_ee='scan')
@@ -62,7 +62,7 @@ pBOLD.head(3)
 # 
 # Create path to the file with all TSNR estimates (previously generated in ```N08_Gather_Results.ipynb```)
 
-# In[21]:
+# In[6]:
 
 
 TSNR_path = osp.join(CODE_DIR,'notebooks','summary_files',f'{DATASET}_TSNR.pkl')
@@ -71,7 +71,7 @@ print('++ TSNR will read from %s' % TSNR_path)
 
 # Load into memory
 
-# In[6]:
+# In[7]:
 
 
 with open(TSNR_path, 'rb') as f:
@@ -80,7 +80,7 @@ with open(TSNR_path, 'rb') as f:
 
 # Rename NORDIC to m-NORDIC so labels agree with the way we refer to magnitude-only NORDIC throughout the manuscript
 
-# In[22]:
+# In[8]:
 
 
 TSNR[('cov','TSNR (Full Brain)')].columns=['Subject','Session','Pre-processing','m-NORDIC','TSNR (Full Brain)']
@@ -93,7 +93,7 @@ TSNR[('corr','TSNR (Full Brain)')].columns=['Subject','Session','Pre-processing'
 # We now combine all loaded data into a single dictionary
 # 
 
-# In[23]:
+# In[9]:
 
 
 QC_metrics = TSNR.copy()
@@ -105,7 +105,7 @@ QC_metrics[FC_METRIC,'pBOLD'] = pBOLD
 # 
 # In Panel a we show Full Brain TSNE across all pipelines. We show statistics based on the Mann-Whitney test. Finally, we include both sessions (1 and 2) by setting ```SES='all'```
 
-# In[24]:
+# In[10]:
 
 
 QC_METRIC = 'TSNR (Full Brain)'
@@ -116,7 +116,7 @@ SES = 'all'
 
 # We create panel a, bar plots for TSNR, using the get_static_report function
 
-# In[25]:
+# In[11]:
 
 
 if SES == 'all':
@@ -145,7 +145,7 @@ panel_a
 # 
 # Similar to section 4, but this time we work with pBOLD instead of TSNR
 
-# In[26]:
+# In[12]:
 
 
 QC_METRIC     = 'pBOLD'
@@ -154,7 +154,7 @@ STAT_TEST     = 'Mann-Whitney'
 SES           = 'all'
 
 
-# In[27]:
+# In[13]:
 
 
 if SES == 'all':
@@ -183,13 +183,13 @@ panel_b
 # 
 # We now add the top black banner to get the final version of the figure.
 
-# In[28]:
+# In[14]:
 
 
 import panel as pn
 
 
-# In[29]:
+# In[15]:
 
 
 fig_banner = pn.pane.HTML(
@@ -211,13 +211,13 @@ fig_banner = pn.pane.HTML(
 )
 
 
-# In[30]:
+# In[16]:
 
 
 figure04 = pn.Column(fig_banner, pn.Row(panel_a,panel_b), width=1750)
 
 
-# In[33]:
+# In[17]:
 
 
 figure04.save('./figures/pBOLD_Figure04.html')
@@ -226,3 +226,73 @@ figure04.save('./figures/pBOLD_Figure04.html')
 # Here is the final figure in static form (for github rendering)
 # 
 # ![Figure 04](figures/pBOLD_Figure04.png)
+
+# ***
+# # Additional Code to generate Suppl. Figure 3.c (TSNR as a function of m-Nordic)
+
+# In[22]:
+
+
+QC_METRIC = 'TSNR (Full Brain)'
+if SES == 'all':
+    data_to_show = QC_metrics[FC_METRIC,QC_METRIC].set_index('Pre-processing').loc[SEL_PIPELINES].reset_index()
+else:
+    idx = pd.IndexSlice
+    data_to_show = QC_metrics[FC_METRIC,QC_METRIC].set_index(['Pre-processing','Session']).loc[idx[SEL_PIPELINES, SES], :].reset_index()
+panel_c = get_static_report(data_to_show,
+                  FC_METRIC,
+                  QC_METRIC,  
+                  x='Pre-processing',
+                  hue='m-NORDIC',
+                  stat_test=STAT_TEST, 
+                  show_stats=True, 
+                  show_points=True,
+                  stat_annot_type='star', 
+                  remove_outliers_from_swarm=True, 
+                  legend_location='lower left', 
+                  session=SES, dot_size=1)
+panel_c.suptitle("(c)", x=0.01, ha="left", fontsize=20);
+panel_c
+
+
+# In[23]:
+
+
+panel_c.tight_layout()
+panel_c.savefig('./figures/pBOLD_SuppFig03_c.png', bbox_inches="tight", pad_inches=0)
+
+
+# ***
+# # Additional Code to generate Suppl. Figure 3.d (pBOLD as a function of m-Nordic)
+
+# In[24]:
+
+
+QC_METRIC='pBOLD'
+if SES == 'all':
+    data_to_show = QC_metrics[FC_METRIC,QC_METRIC].set_index('Pre-processing').loc[SEL_PIPELINES].reset_index()
+else:
+    idx = pd.IndexSlice
+    data_to_show = QC_metrics[FC_METRIC,QC_METRIC].set_index(['Pre-processing','Session']).loc[idx[SEL_PIPELINES, SES], :].reset_index()
+panel_d = get_static_report(data_to_show,
+                  FC_METRIC,
+                  QC_METRIC,  
+                  x='Pre-processing',
+                  hue='m-NORDIC',
+                  stat_test=STAT_TEST, 
+                  show_stats=True, 
+                  show_points=True,
+                  stat_annot_type='star', 
+                  remove_outliers_from_swarm=True, 
+                  legend_location='lower left', 
+                  session=SES, dot_size=1)
+panel_d.suptitle("(d)", x=0.01, ha="left", fontsize=20);
+panel_d
+
+
+# In[25]:
+
+
+panel_d.tight_layout()
+panel_d.savefig('./figures/pBOLD_SuppFig03_d.png', bbox_inches="tight", pad_inches=0)
+
